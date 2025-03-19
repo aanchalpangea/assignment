d349e952c08067a522a9bbcdad455d582dd5ac96
Chandra Prakash <131661563+chandra-pangea@users.noreply.github.com>
2025-03-19 17:10:24 +0530

    test: testing husky configuration

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   console.log("PPPPPPPPPPPPPPPPP");
   console.log("PPPPPPPPPPPPPPPPPLLLL");
+  console.log("PPPPPPPPPPPPPPPPPLLLL");
   return (
     <header className="bg-blue-500 py-4 text-white text-lg">
       this is header
