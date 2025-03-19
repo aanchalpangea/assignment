2492a270149a77744e2da527422a31ac7e495359
Chandra Prakash <131661563+chandra-pangea@users.noreply.github.com>
2025-03-19 17:30:20 +0530

    test

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "src/**/*.{js,jsx,ts,tsx}": ["eslint --fix --quiet", "prettier --write"],
-  "*.{json,css,md}": ["prettier --write"],
-};
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const Header = () => {
     return (
         <header className="bg-blue-500 py-4 text-white text-lg">
