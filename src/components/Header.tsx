65a21c00bbe03de9a2f05d9333c9a41baac99375
Chandra Prakash <131661563+chandra-pangea@users.noreply.github.com>
2025-03-19 16:24:15 +0530

    test

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  console.log("testing husky");
+  return (
+    <header className="bg-blue-500 py-4 text-white text-lg">
+      this is header
+    </header>
+  );
+};
+
+export default Header; 
